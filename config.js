a9eec9addb003d5c938cfdfad74efae5a10d708d
Gustavo Rodrigues <g.rodrigues@global.ntt>
2021-03-20 14:36:16 -0300

    Weather App

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mapboxToken: "pk.eyJ1IjoiZ3Vzcm9kcmlndWVzcCIsImEiOiJja2tvYmRxdTYwYmt4Mnd0bzlpMHd1aXl0In0.O4l7gcU-pTagcsq6iF0k-A",
+    weatherStackKey: "df234fb3d1b94afe3875ce9fbfc2bc39"
+};
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request')
+const config = require('../../config')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=' + config.weatherStackKey + '&query=' + latitude + ',' + longitude;
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
